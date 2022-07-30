17b072e6f3b2eaacdc26bf6504e75704511f1b56
Guo Zhi <qtxuning1999@sjtu.edu.cn>
2022-07-30 20:06:59 +0800

    metal

--- c++/main.cpp
+++ c++/main.cpp
@@ -47,9 +47,8 @@ int main() {
     world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(make_shared<sphere>(point3( 0.0, 0.0, -1.0), 0.5, material_center));
     world.add(make_shared<sphere>(point3( -1.0, 0.0, -1.0), 0.5, material_left));
-    world.add(make_shared<sphere>(point3( -1.0, 0.0, -1.0), 0.5, material_right));
+    world.add(make_shared<sphere>(point3( 1.0, 0.0, -1.0), 0.5, material_right));
     
-
     // Camera
     camera cam;
 
