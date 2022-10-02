4627970fff06f54e1825e393753145207ec5c6d6
Guo Zhi <qtxuning1999@sjtu.edu.cn>
2022-10-02 18:36:16 +0800

    11.2 positioning and orienting the camera

--- c++/main.cpp
+++ c++/main.cpp
@@ -37,17 +37,21 @@ int main() {
     const int max_depth = 50;
 
     // World
-    auto R = cos(pi/4);
     hittable_list world;
 
-    auto material_left = make_shared<lambertian>(color(0,0,1));
-    auto material_right = make_shared<lambertian>(color(1,0,0));
+    auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+    auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
+    auto material_left = make_shared<dielectric>(1.5);
+    auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
 
-    world.add(make_shared<sphere>(point3(-R, 0, -1), R, material_left));
-    world.add(make_shared<sphere>(point3( R, 0, -1), R, material_right));
+    world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
+    world.add(make_shared<sphere>(point3( 0.0, 0.0, -1.0), 0.5, material_center));
+    world.add(make_shared<sphere>(point3( -1.0, 0.0, -1.0), 0.5, material_left));
+    world.add(make_shared<sphere>(point3( -1.0, 0.0, -1.0), -0.45, material_left));
+    world.add(make_shared<sphere>(point3( 1.0, 0.0, -1.0), 0.5, material_right));
     
     // Camera
-    camera cam(90.0, aspect_ratio);
+    camera cam(point3(-2,2,1), point3(0,0,-1), vec3(0,1,0), 90, aspect_ratio);
 
     // Render
     std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
